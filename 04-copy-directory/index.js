4b427ed36e7b2bba6245595220f5b86420884d88
gorobchenko-k <gorobchenko.es@mail.ru>
2023-05-04 12:15:20 +0300

    feat: finish 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirPath = path.join(__dirname, 'files');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+  if (err) throw err;
+
+  fs.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      fs.unlink(path.join(__dirname, 'files-copy', file.name), err => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      if (!file.isDirectory()) {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), err => {
+          if (err) throw err;
+          console.log('Файл успешно скопирован');
+        });
+      }
+    });
+  });
+});
